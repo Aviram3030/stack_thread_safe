fdc388b9dcf99357c18ee5f962cc04a5e7a72583
unknown <aviram3030@gmail.com>
2022-01-18 13:31:29 +0200

    adding ReadWriteLocks with conditions

--- stack_thread_safe/src/experis/ds/BlockingStack.java
+++ stack_thread_safe/src/experis/ds/BlockingStack.java
@@ -1,8 +1,13 @@
 package experis.ds;
 
+import java.util.concurrent.locks.*;
+
 public class BlockingStack<T> implements Stack<T>{
-    private T[] stack;
+    private final T[] stack;
     private int size = 0;
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Condition full = lock.writeLock().newCondition();
+    private final Condition empty = lock.writeLock().newCondition();
 
     public BlockingStack(int capacity) {
         stack = (T[])new Object[capacity];
@@ -10,26 +15,44 @@ public class BlockingStack<T> implements Stack<T>{
 
     @Override
     public void push(T val) {
-        if(!isFull()){
-            stack[size++] = val;
+        lock.writeLock().lock();
+        while(isFull()){
+            try {
+                full.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
+        stack[size++] = val;
+        lock.writeLock().unlock();
     }
 
     @Override
     public T pop() {
-        if(!isEmpty()){
+        lock.writeLock().lock();
+        while(isEmpty()){
+            try {
+                empty.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
             return stack[--size];
         }
+        finally {
+            lock.writeLock().unlock();
+        }
 
-        return null;
     }
 
     @Override
     public T peek() {
+        lock.readLock().lock();
         if(!isEmpty()){
             return stack[size - 1];
         }
-
+        lock.readLock().unlock();
         return null;
     }
     
--- stack_thread_safe/src/experis/ds/Main.java
+++ stack_thread_safe/src/experis/ds/Main.java
@@ -1,9 +1,8 @@
 package experis.ds;
 
-import java.util.Stack;
-
 public class Main {
 
     public static void main(String[] args) {
+	// write your code here
     }
 }
